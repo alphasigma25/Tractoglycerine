dd09ee877b724cd4dd5d219c2acecd728da0eade
LauG18 <140497526+LauG18@users.noreply.github.com>
2023-07-26 16:29:45 +0200

    Player death

--- Assets/scripts/Player/Player.cs
+++ Assets/scripts/Player/Player.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public float MaxHealth = 3;
-    private float ActualHealth;
-    private float dammage; 
+    public float maxHealth = 100;
+    private float health;
+    private bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        health = maxHealth;
         
     }
+    void MakeDamage (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Debug.Log("Le joueur est mort");
+            gameOver = true;
+            SceneManager.LoadScene("GameOver");
+        }
+    }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+          //  MakeDamage(collision.gameObject.GetComponent <Enemy> ().damage);
+            
+        }
+    }
+        // Update is called once per frame
+        void Update()
     {
 
-       // if ()
+       
     }
 }
