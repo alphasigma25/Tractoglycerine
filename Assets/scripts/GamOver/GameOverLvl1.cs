fdee33015da52c4b0147bbf77f85c3792ee1edd0
VictorMSilva94 <140509823+VictorMSilva94@users.noreply.github.com>
2023-07-28 16:51:12 +0200

    Update after revert 16:51

---
+++ Assets/scripts/GamOver/GameOverLvl1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverLvl1 : MonoBehaviour
+{
+    // Called when we click the "Restart" button.
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("Level 1 calvin");
+    }
+    // Called when we click the "Quit" button.
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
