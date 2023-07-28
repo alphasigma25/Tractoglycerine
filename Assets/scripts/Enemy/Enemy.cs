4a298cde20538d6799b141ee4e026f2a082e8a30
dragonlab <calvin.gehrig@gmail.com>
2023-07-28 22:49:35 +0200

    Immeubles, tractoboule et rouleau

--- Assets/scripts/Enemy/Enemy.cs
+++ Assets/scripts/Enemy/Enemy.cs
@@ -7,9 +7,15 @@ public class Enemy : MonoBehaviour
 {
     private Transform playerTransfo;
     private Vector3 direction;
-    public int health /*{ get;  private set; }*/ = 1; //un hack car quand objet créé, health a pas encore été init (donc =0) et on va le supprimer directement dans le manager
+    public int health /*{ get;  private set; }*/ = 1; //un hack car quand objet cr??, health a pas encore ?t? init (donc =0) et on va le supprimer directement dans le manager
     public EnemyData data { get; set; }
 
+    [SerializeField] private Transform objectToRotate;
+    [SerializeField] private float rotationAmplitude = 20f;
+    [SerializeField] private float rotationSpeed = 0.001f;
+
+    private float timer = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +31,18 @@ public class Enemy : MonoBehaviour
         direction = (playerTransfo.position - transform.position).normalized;
         transform.position += direction * data.speed * Time.deltaTime;
         transform.LookAt(playerTransfo.position, Vector3.back);
+
+        timer += Time.deltaTime;
+        float rotation = Mathf.Sin(timer * rotationSpeed);
+        rotation *= rotationAmplitude;
+        rotation -= 90f;
+        Debug.Log(rotation);
+        Vector3 oldRotation = objectToRotate.localRotation.eulerAngles;
+        oldRotation.x = rotation;
+        oldRotation.y = 90f;
+        oldRotation.z = -90f;
+        objectToRotate.localRotation = Quaternion.Euler(oldRotation);
+        Debug.Log(objectToRotate.localRotation);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
